cfb90274f76c065bf4c26572167ca6b30b19c168
Dominik <marczukd@gmail.com>
2023-07-05 09:00:26 +0200

    add connection between discovery and user tables in databases and modify Discovery class

--- src/main/java/pl/readstack/domain/discovery/Discovery.java
+++ src/main/java/pl/readstack/domain/discovery/Discovery.java
@@ -9,14 +9,25 @@ public class Discovery {
     private String description;
     private LocalDateTime dateAdded;
     private Integer categoryId;
+    private Integer userId;
 
-    public Discovery(Integer id, String title, String url, String description, LocalDateTime dateAdded, Integer categoryId) {
+    public Discovery(Integer id, String title, String url, String description, LocalDateTime dateAdded, Integer categoryId, Integer userId) {
         this.id = id;
         this.title = title;
         this.url = url;
         this.description = description;
         this.dateAdded = dateAdded;
         this.categoryId = categoryId;
+        this.userId = userId;
+    }
+
+    public Discovery(String title, String url, String description, LocalDateTime dateAdded, Integer categoryId, Integer userId) {
+        this.title = title;
+        this.url = url;
+        this.description = description;
+        this.dateAdded = dateAdded;
+        this.categoryId = categoryId;
+        this.userId = userId;
     }
 
     public Integer getId() {
@@ -41,5 +52,13 @@ public class Discovery {
 
     public Integer getCategoryId() {
         return categoryId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 }
