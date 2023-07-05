21c1e1d6046e2f7b3ff61dd1f289a42242c8798a
Dominik <marczukd@gmail.com>
2023-07-05 09:47:03 +0200

    add method add in DiscoveryService and modify mapper

--- src/main/java/pl/readstack/domain/api/DiscoveryService.java
+++ src/main/java/pl/readstack/domain/api/DiscoveryService.java
@@ -2,12 +2,20 @@ package pl.readstack.domain.api;
 
 import pl.readstack.domain.discovery.Discovery;
 import pl.readstack.domain.discovery.DiscoveryDao;
+import pl.readstack.domain.discovery.UserDao;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DiscoveryService {
     private final DiscoveryDao discoveryDao = new DiscoveryDao();
+    private final DiscoveryMapper discoveryMapper = new DiscoveryMapper();
+
+    public void add(DiscoverySaveRequest saveRequest) {
+        Discovery discoveryToSave = discoveryMapper.map(saveRequest);
+        discoveryDao.save(discoveryToSave);
+    }
 
     public List<DiscoveryBasicInfo> findAll() {
         return discoveryDao.findAll()
@@ -22,12 +30,26 @@ public class DiscoveryService {
     }
 
     private static class DiscoveryMapper {
+        private final UserDao userDao = new UserDao();
         static DiscoveryBasicInfo map (Discovery d) {
             return new DiscoveryBasicInfo(
                     d.getTitle(),
                     d.getUrl(),
                     d.getDescription(),
                     d.getDateAdded()
+            );
+        }
+
+        Discovery map (DiscoverySaveRequest ds) {
+            return new Discovery(
+                    ds.getTitle(),
+                    ds.getUrl(),
+                    ds.getDescription(),
+                    LocalDateTime.now(),
+                    ds.getCategoryId(),
+                    userDao.findByUsername(ds.getAuthor())
+                            .orElseThrow()
+                            .getId()
             );
         }
     }
